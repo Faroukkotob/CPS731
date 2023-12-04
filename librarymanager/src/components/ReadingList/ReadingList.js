ae7a7ee4f3b3ce2143e9311bf9a0fb6b44331979
James W <bubb4h0t3p@gmail.com>
2023-12-04 07:01:58 -0500

    pt2

--- librarymanager/src/components/ReadingList/ReadingList.js
+++ librarymanager/src/components/ReadingList/ReadingList.js
@@ -6,23 +6,77 @@ import '@fontsource/roboto/700.css';
 
 import Navbar from "../Navbar/Navbar";
 import React from "react";
-import { DataGrid } from '@mui/x-data-grid';
+import { DataGrid, useGridApiContext } from '@mui/x-data-grid';
+import { Button } from '@mui/material';
+
+function getCatalogue() {
+    var savedBooks = JSON.parse(localStorage.getItem('books'));
+    const preloadBooks = require("../../preloaded_books/preloaded_book_info.json");
+    if (savedBooks) {
+      for (var preloadedBookId in preloadBooks) {
+        if(!savedBooks.includes(preloadBooks[preloadedBookId])) {
+          savedBooks.push(preloadBooks[preloadedBookId]);
+        }
+      }
+      return savedBooks;
+    } else {
+      return preloadBooks;
+    }
+}
+
+function getReadingListBooks() {
+    return JSON.parse(localStorage.getItem('readingListBooks'));
+}
+
+const columns = [
+    { field: 'id', headerName: 'ID', width: 70 },
+    { field: 'title', headerName: 'Title', width: 200 },
+    { field: 'isbn', headerName: 'Last name', width: 200 },
+    { field: 'author', headerName: 'Author', width: 200 },
+    { field: 'genre', headerName: 'Genre', width: 200 }
+];
 
 const ReadingList = () => {
     
-    var loadedBooks = require("../../preloaded_books/preloaded_book_info.json");
-    const columns = [
-        { field: 'id', headerName: 'ID', width: 70 },
-        { field: 'title', headerName: 'Title', width: 200 },
-        { field: 'isbn', headerName: 'Last name', width: 200 },
-        { field: 'author', headerName: 'Author', width: 200 },
-        { field: 'genre', headerName: 'Genre', width: 200 }
-    ];
-    var rows = [];
-    for (var bookId in loadedBooks) {
-        const thisBook = loadedBooks[bookId];
-        rows.push({id: thisBook.id, title: thisBook.title, isbn: thisBook.isbn, author: thisBook.author, genre: thisBook.genre});
+    var bookCatalogue = getCatalogue();
+    var catalogueRows = [];
+    for (var bookIdCatalogue in bookCatalogue) {
+        const thisBook = bookCatalogue[bookIdCatalogue];
+        catalogueRows.push({id: thisBook.id, title: thisBook.title, isbn: thisBook.isbn, author: thisBook.author, genre: thisBook.genre});
     }
+    
+    //var catalogueTableRef = useGridApiContext();
+    var catalogueTable = (<DataGrid
+        rows={catalogueRows}
+        columns={columns}
+        initialState={{
+        pagination: {
+        paginationModel: { page: 0, pageSize: 5 },
+        },
+        }}
+        pageSizeOptions={[5, 10]}
+        checkboxSelection
+    />);
+    var readingListBooks = getReadingListBooks();
+    var readingListRows = [];
+    for (var bookIdReadingList in readingListBooks) {
+        const thisBook = bookCatalogue[bookIdReadingList];
+        readingListRows.push({id: thisBook.id, title: thisBook.title, isbn: thisBook.isbn, author: thisBook.author, genre: thisBook.genre});
+    }
+    
+   // var readingTableRef = useGridApiContext();
+    var readingListTable = (<DataGrid
+        rows={readingListRows}
+        columns={columns}
+        initialState={{
+        pagination: {
+        paginationModel: { page: 0, pageSize: 5 },
+        },
+        }}
+        pageSizeOptions={[5, 10]}
+        checkboxSelection
+    />
+    );
     return (
         <div className="readingList">
           <Navbar />
@@ -31,20 +85,15 @@ const ReadingList = () => {
               <h1>Reading List</h1>
             </header>
             <main>
-            <DataGrid
-                rows={rows}
-                columns={columns}
-                initialState={{
-                pagination: {
-                paginationModel: { page: 0, pageSize: 5 },
-                },
-            }}
-            pageSizeOptions={[5, 10]}
-            checkboxSelection
-            />
+            {catalogueTable}
+            <Button variant="text" onClick={() => {
+            }}>Add Books</Button>
+            <Button variant="text" onClick={() => {
+
+            }}>Remove Books</Button>
             </main>
             <footer className="readingList-footer">
-              {/* Footer content here */}
+            {readingListTable}
             </footer>
           </div>
         </div>
