a9c613b0f7edbb66631d99da8816af802eaf35cf
Faroukkotob <68594444+Faroukkotob@users.noreply.github.com>
2023-12-04 13:02:04 -0500

    Update ReadingList.js

--- librarymanager/src/components/ReadingList/ReadingList.js
+++ librarymanager/src/components/ReadingList/ReadingList.js
@@ -8,6 +8,9 @@ import Navbar from "../Navbar/Navbar";
 import React from "react";
 import { DataGrid } from '@mui/x-data-grid';
 import { Button } from '@mui/material';
+import { useNavigate } from "react-router-dom";
+import { getFirestore, collection, addDoc, serverTimestamp } from 'firebase/firestore';
+import { auth } from "../Auth/Auth";
 
 function getCatalogue() {
     var savedBooks = JSON.parse(localStorage.getItem('books'));
@@ -40,8 +43,47 @@ const columns = [
 var readingListSelected = new Set();
 var catalogueSelected = new Set();
 
+const addToCart = async (booksToAdd) => {
+  try {
+    const db = getFirestore();
+    const cartCollection = collection(db, 'cart');
+
+    const booksData = booksToAdd.map((book) => ({
+      userId: auth.currentUser.uid,
+      bookId: book.id,
+      title: book.title,
+      isbn: book.isbn,
+      author: book.author,
+      genre: book.genre,
+      price: book.price,
+      timestamp: serverTimestamp(),
+    }));
+
+    // Add the selected books to the 'cart' collection
+    await Promise.all(booksData.map((bookData) => addDoc(cartCollection, bookData)));
+
+    console.log('Books successfully added to the cart');
+  } catch (error) {
+    console.error('Error adding books to the cart:', error);
+  }
+};
+
+
 
 const ReadingList = () => {
+  const navigate = useNavigate();
+  const handleAddToCart = () => {
+    const selectedCatalogueBooks = catalogueRows.filter((book) =>
+      catalogueSelected.has(book.id) && readingListRows.every((alreadyOnList) => alreadyOnList.id !== book.id)
+    );
+
+    // Call the addToCart function to send the selected books to the Firebase cart
+    addToCart(selectedCatalogueBooks);
+
+    // Update the reading list state
+    setReadingListRows((prevRows) => [...prevRows, ...selectedCatalogueBooks]);
+  };
+
   
     var [catalogueRows,setCatalogueRows] = React.useState([]);
     var [readingListRows, setReadingListRows] = React.useState([]);
@@ -97,36 +139,34 @@ const ReadingList = () => {
     />
     );
     return (
-        <div className="readingList">
-          <Navbar />
+      <div className="readingList">
+        <Navbar />
         <div className="main-content">
-        <header className="readingList-header">
-        <h1>Reading List</h1>
-        <h2>Catalogue</h2>
-        </header>
-            <main>
-        {catalogueTable}
-            <Button variant="text" onClick={() => {
-              const selectedCatalogueBooks = catalogueRows.filter((book) => {
-                console.log("Book selection");
-                console.log(book);
-                return catalogueSelected.has(book.id) && readingListRows.every((alreadyOnList) => alreadyOnList.id !== book.id);
-              })
-              readingListRows = [...readingListRows, ...selectedCatalogueBooks];
-              setReadingListRows(readingListRows);
-            }}>Add Books</Button>
-            </main>
-            <h2>My List</h2>
-            <footer className="readingList-footer">
+          <header className="readingList-header">
+            <h1>Reading List</h1>
+            <h2>Catalogue</h2>
+          </header>
+          <main>
+            {catalogueTable}
+            <Button variant="text" onClick={handleAddToCart}>
+              Add Books
+            </Button>
+          </main>
+          <h2>My List</h2>
+          <footer className="readingList-footer">
             {readingListTable}
             <Button variant="text" onClick={() => {
-              readingListRows = readingListRows.filter((book) =>
-               !readingListSelected.has(book.id));
-              setReadingListRows(readingListRows);
-            }}>Remove Books</Button>
-            </footer>
-          </div>
+              const updatedReadingList = readingListRows.filter((book) =>
+                !readingListSelected.has(book.id)
+              );
+              setReadingListRows(updatedReadingList);
+            }}>
+              Remove Books
+            </Button>
+          </footer>
         </div>
+      </div>
     );
-}
+  }
 export default ReadingList;
+
